ef5faa09a0dfb813af19fd366d3e127310682d07
JuanBautistaAguin <126636931+JuanBautistaAguin@users.noreply.github.com>
2023-03-01 01:18:10 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.body.scrollTop = 1000;
+document.getElementById("btn_enviar").addEventListener("click", myFunction);
+
+function myFunction() {
+  alert ("Esto es un formulario de contacto falso.");
+}
